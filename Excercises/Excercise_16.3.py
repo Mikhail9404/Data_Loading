361cca9ebb8d851a2a33f4cbd02cb0b862c6908b
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-08-07 19:21:42 +0300

    Excercise_16.3.py finished

--- Excercises/Excercise_16.3.py
+++ Excercises/Excercise_16.3.py
@@ -2,8 +2,9 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename_1 = 'C:/Users/kuznecov.mihail/PycharmProjects/Data_Loading/data/sitka_weather_2018_simple.csv'
-filename_2 = 'C:/Users/kuznecov.mihail/PycharmProjects/Data_Loading/data/death_valley_2018_simple.csv'
+filename_1 = '../data/sitka_weather_2018_simple.csv'
+filename_2 = '../data/death_valley_2018_simple.csv'
+filename_3 = '../data/san_francisco_2018_simple.csv'
 
 current_date = range(1, 364)
 
@@ -36,16 +37,32 @@ with open(filename_2) as f:
         else:
             average_dv.append((high + low) / 2)
 
+with open(filename_3) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Получение средней температуры из файла (San Francisco).
+    average_san_fr = []
+    for row in reader:
+        try:
+            aver_san_fr = int(row[3])
+        except ValueError:
+            print(f"Missing data for {current_date} (Death Valley)")
+        else:
+            average_san_fr.append(aver_san_fr)
+
+
 # Нанесение данных на диаграмму.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(average_sitka, c='red', alpha=0.5)
 ax.plot(average_dv, c='blue', alpha=0.5)
+ax.plot(average_san_fr, c='green', alpha=0.5)
 
 # Форматирование даиграммы.
-title = "Daily average temperatures - 2018\nSitka and Death Valley"
-plt.title(title, fontsize=20)
-plt.xlabel('', fontsize=16)
+title = "Daily average temperatures - 2018\nSitka - Red\nDeath Valley - Blue\nSan Francisco - Green"
+plt.title(title, fontsize=8)
+plt.xlabel('', fontsize=5)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
