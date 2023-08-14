390d2c6dea81731ef308f6cd9056f54be81232c5
Mikhail <Misha9404@yandex.ru>
2023-08-14 12:03:41 +0300

    Refactoring eq_world_map.py

--- eq_world_map.py
+++ eq_world_map.py
@@ -10,14 +10,12 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
 # Нанесение данных на карту.
 # data = [Scattergeo(lon=lons, lat=lats)]
@@ -25,6 +23,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
